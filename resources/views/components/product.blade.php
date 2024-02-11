7480b10ce17f458348141d20f4293d4d2c4a2bd6
iranmilad <kazemi.milad@gmail.com>
2024-02-11 06:00:14 +0330

    a

--- resources/views/components/product.blade.php
+++ resources/views/components/product.blade.php
@@ -1,20 +1,20 @@
-<a class="product {{ isset($mobile) ? 'product-mobile' : ''}} {{$image2 ? 'product-two-image' : ''}} {{!$nobtn ? 'show-btn' : ''}}" href="{{$link}}">
+<a class="product {{ isset($mobile) ? 'product-mobile' : ''}} {{ isset($image2) ? 'product-two-image' : ''}} {{!isset($nobtn) ? 'show-btn' : ''}}" href="{{  $link ?: '' }}">
     <!-- The whole future lies in uncertainty: live immediately. - Seneca -->
-    <img class="thumbnail" src="{{ $image ?: Vite::asset('resources/images/image-broken.png') }}" alt="{{ $name }}">
-    <img class="thumbnail-2" src="{{ $image2 ?: '' }}" alt="{{ $name }}">
+    <img class="thumbnail" src="{{ $image ?: Vite::asset('resources/images/image-broken.png') }}" alt="{{ isset($name) ?: '' }}">
+    <img class="thumbnail-2" src="{{ $image2 ?: '' }}" alt="{{ $name ?: ''}}">
     <div class="body">
         <span class="title tw-text-right tw-w-full tw-font-semibold">{{$name}}</span>
         @if(!isset($available))
             @if(isset($discountedPrice))
                 <div class="tw-w-full  tw-center tw-justify-between">
-                    <span class="badge tw-bg-red-500 rounded-pill">{{ $discount }}</span>
-                    <span class="tw-text-sm">{{$price}}
+                    <span class="badge tw-bg-red-500 rounded-pill">{{ $discount ?: ''}}</span>
+                    <span class="tw-text-sm">{{$price ?: ''}}
                     <svg style="width: 16px; height: 16px; fill: var(--undefined);">
                         <use xlink:href="#toman"></use>
                     </svg>
                     </span>
                 </div>
-                <span class="tw-line-through tw-text-gray-400 tw-text-xs tw-pl-[22px] tw-text-left tw-w-full">{{$discountedPrice}}</span>
+                <span class="tw-line-through tw-text-gray-400 tw-text-xs tw-pl-[22px] tw-text-left tw-w-full">{{$discountedPrice ?: ''}}</span>
                 @if(isset($stock))
                     <span class="tw-text-xs tw-text-red-500 tw-text-right tw-w-full">
                         <i class="fa-duotone fa-store"></i>
