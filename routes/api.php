3a5fcfedcf11c9f84e6f411b524c763448c3d150
iranmilad <kazemi.milad@gmail.com>
2024-03-16 18:26:03 +0330

    update api

--- routes/api.php
+++ routes/api.php
@@ -7,9 +7,8 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\View;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ApiController;
-use App\Http\Controllers\CategoryController;
 use Illuminate\Support\Facades\Storage;
-use App\Http\Controllers\ProductController;
+use App\Http\Controllers\CategoryController;
 
 
 /*
@@ -22,6 +21,302 @@ use App\Http\Controllers\ProductController;
 | be assigned to the "api" middleware group. Make something great!
 |
 */
+
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    return $request->user();
+});
+
+
+Route::get('card/{id}', function ($id) {
+    $data = [
+        'img' => "resources/images/single-product/6.jpg",
+        'title' => 'پرده پانچی آماده هازان (کتان) رنگ خردلی',
+        'price' => '12,800,000',
+        'discounted_price' => '11,000,000', // 10.99 - (10.99 * 0.2) = 8.79
+        'discount' => "20%",
+        'options' => [
+            'سایز' => '2*3',
+            'رنگ' => 'خردلی',
+            'عرض' => '2.6 متر',
+            'طول' => '3.5 متر',
+            'هدریل' => 'داخلی',
+            'حالت بالابر' => 'دستی',
+            'همراه با موتور' => 'ندارد',
+            'گارانتی' => 'سه سال محدود'
+        ]
+    ];
+
+    // Render the Blade view
+    $html = View::make('components/cart-details', $data)->render();
+
+    // Return the HTML as an API response
+    return response()->json(['html' => $html]);
+});
+
+Route::post('/file/upload', function (Request $request) {
+    $uploadedFiles = [];
+
+    foreach ($request->file('files') as $file) {
+        $uuid = Uuid::uuid4()->toString();
+        $uploadedFiles[] = [
+            'uuid' => $uuid,
+            'original_name' => $file->getClientOriginalName(),
+            'mime_type' => $file->getMimeType(),
+        ];
+    }
+
+    return response()->json(['files' => $uploadedFiles], 200);
+});
+
+// FOR MESSAGE IN DASHBOARD
+Route::get('/messages/{id}', function ($id) {
+    $message = [
+        'id' => $id,
+        'sender' => 'مدیریت',
+        'priority' => 'زیاد',
+        'title' => 'پیام اول',
+        'messages' => [
+            [
+                'id' => 1,
+                'message' => 'پیام اول',
+                'created_at' => '1400/01/01 12:00:00',
+                'files' => ['https://placehold.co/600x400','https://placehold.co/600x400'],
+                'you' => true
+            ],
+            [
+                'id' => 2,
+                'message' => 'پیام دوم',
+                'created_at' => '1400/01/01 12:00:00',
+                'files' => ['https://placehold.co/600x400','https://placehold.co/600x400'],
+                'you' => false
+            ],
+        ],
+    ];
+
+    return response()->json(['message' => $message]);
+});
+
+// FOR IMAGE DOT. IT GETS DATA OF PRODUCT {ID} AND FILTER
+Route::get("/imgdot/{id}",function($id){
+    $product = [
+        "name" => "محصول",
+        "img" => "https://picsum.photos/200",
+        "price" => "25,000,000",
+        "discounted_price" => "18,000,000",
+        "discount" => "20%"
+    ];
+
+    $html = View::make("components/imgdot",$product)->render();
+
+    return response()->json(['html' => $html]);
+});
+
+
+/**
+ * this route returns the product details and update price and time and discount and also the images of product.
+ *
+ */
+Route::post("/product",function(Request $request){
+
+    /**
+     * regular_price & sale_price & discount & time_delivery are optional.
+     * images are required. if they are not exist, use a default image.
+     */
+    $response = [
+        "name" => $request->name,
+        "images" => [
+            "/images/5.jpg",
+            "/images/6.jpg",
+        ],
+        "regular_price" => "25,000,000",
+        "sale_price" => "18,000,000",
+        "discount" => "20%",
+        "time_delivery" => 2
+    ];
+
+    /**
+     * [ Unavailable product ]
+     */
+    // $response = [
+    //     "name" => $request->name,
+    //     "images" => [
+    //         "https://placehold.co/900?text=2",
+    //     ],
+    // ];
+
+    return response()->json($response);
+});
+
+Route::post("/add-to-cart",function(Request $request){
+    $response = [
+        "status" => "success",
+        "message" => "محصول با موفقیت به سبد خرید اضافه شد.",
+        "cart" => [
+            "count" => 5,
+        ],
+    ];
+
+    return response()->json($response);
+});
+
+Route::post("/remove-cart",function(Request $request){
+    $response = [
+        "status" => "success",
+        "message" => "محصول با موفقیت از سبد خرید حذف شد.",
+        "cart" => [
+            "count" => 2,
+            "total" => "15,000,000",
+            "profit" => "5,000,000",
+            "discounts" => "20%"
+        ],
+    ];
+
+    // empty cart after removing the last item
+    // $response = [
+    //     "status" => "success",
+    //     "message" => "محصول با موفقیت از سبد خرید حذف شد.",
+    //     "cart" => array(),
+    // ];
+
+    // error response
+    // $response = [
+    //     "status" => "error",
+    //     "message" => "محصول در سبد خرید یافت نشد.",
+    // ];
+
+    return response()->json($response);
+});
+
+Route::post("/remove-all-cart",function(Request $request){
+    $response = [
+        "status" => "success",
+        "message" => "همه محصولات با موفقیت از سبد خرید حذف شد.",
+    ];
+
+    return response()->json($response);
+});
+
+/**
+ * من را خبر کن
+ */
+Route::post("/letmeknow",function(Request $request){
+    $response = [
+        "status" => "success",
+        "message" => "اطلاعات با موفقیت ارسال شد. پس از موجود شدن به شما اطلاع داده خواهد شد.",
+    ];
+
+    return response()->json($response);
+});
+
+/**
+ * add to wishlist
+ */
+Route::post('/wishlist',function(Request $request){
+    $response = [
+        "status" => "success",
+        "message" => "محصول با موفقیت به لیست علاقه مندی ها اضافه شد.",
+    ];
+
+    return response()->json($response);
+});
+
+/**
+ * remove from wishlist
+ */
+Route::delete("/wishlist",function(Request $request){
+    $response = [
+        "status" => "success",
+        "message" => "محصول با موفقیت از لیست علاقه مندی ها حذف شد.",
+    ];
+
+    return response()->json($response);
+});
+
+/**
+ * Category Ajax
+ */
+// Route::post("/category",function(Request $request){
+
+//     /**
+//      * Request
+//      */
+//     // $req = [
+//     //     "url" => "https://localhost:8000/category?category=1?page=1",
+//     // ]
+
+//     /**
+//      * Response
+//      */
+//     $products = [
+//         [
+//             "id" => 1,
+//             "name" => "محصول 1",
+//             "img" => "https://placehold.co/900?text=1",
+//             "regular_price" => "25,000,000",
+//         ],
+//         [
+//             "id" => 2,
+//             "name" => "محصول 2",
+//             "img" => "https://placehold.co/900?text=2",
+//             "regular_price" => "25,000,000",
+//         ],
+//         [
+//             "id" => 3,
+//             "name" => "محصول 3",
+//             "img" => "https://placehold.co/900?text=3",
+//             "regular_price" => "25,000,000",
+//             "sale_price" => "18,000,000",
+//             "discount" => "20%"
+//         ],
+//         [
+//             "id" => 4,
+//             "name" => "محصول 4",
+//             "img" => "https://placehold.co/900?text=4",
+//             "regular_price" => "25,000,000",
+//             "stock" => "5"
+//         ],
+//         [
+//             "id" => 5,
+//             "name" => "محصول 5",
+//             "img" => "https://placehold.co/900?text=5",
+//         ],
+//     ];
+
+//     $html = View::make("components/category-products",compact('products'))->render();
+
+//     // remove options items
+//     // for example this is url: https://localhost:8000/category?material=1&color=blue&page=1
+//     $removeOptions = [
+//         [
+//             "title" => "حذف همه",
+//             "url" => "https://localhost:8000/category", // new url after remove all options
+//         ],
+//         // color
+//         [
+//             "title" => "ویژگی چک باکس : موجود",
+//             "url" => "https://localhost:8000/category?checkbox=1", // new url after remove this option,
+//             // on clicked item
+//             "item" => [
+//                 "name" => "checkbox",
+//                 "value" => "0"
+//             ]
+//         ],
+//         // material
+//         [
+//             "title" => "ویژگی چک باکس : همه",
+//             "url" => "https://localhost:8000/category?checkbox=0", // new url after remove this option
+//             // on clicked item
+//             "item" => [
+//                 "name" => "checkbox",
+//                 "value" => "1"
+//             ]
+//         ],
+//     ];
+
+//     // total is the total number of pages
+//     return response()->json(['html' => $html,'total' => 10,'removeOptions' => $removeOptions]);
+// });
+
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
@@ -287,32 +582,26 @@ Route::post("/category",[CategoryController::class,'list'])->name('api.category'
 
 //     $html = View::make("components/category-products",compact('products'))->render();
 
-    // remove options items
-    // for example this is url: https://localhost:8000/category?material=1&color=blue&page=1
-    // $removeOptions = [
-    //     [
-    //         "title" => "حذف همه",
-    //         "url" => "https://localhost:8000/category", // new url after remove all options
-    //     ],
-    //     // color
-    //     [
-    //         "title" => "ویژگی چک باکس : موجود",
-    //         "url" => "https://localhost:8000/category?checkbox=1", // new url after remove this option,
-    //         // on clicked item
-    //         "item" => [
-    //             "name" => "checkbox",
-    //             "value" => "0"
-    //         ]
-    //     ],
-    //     // material
-    //     [
-    //         "title" => "ویژگی چک باکس : همه",
-    //         "url" => "https://localhost:8000/category?checkbox=0", // new url after remove this option
-    //         // on clicked item
-    //         "item" => [
-    //             "name" => "checkbox",
-    //             "value" => "1"
-    //         ]
-    //     ],
-    // ];
+//     // remove options items
+//     // for example this is url: https://localhost:8000/category?material=1&color=blue&page=1
+//     $removeOptions = [
+//         [
+//             "title" => "حذف همه",
+//             "url" => "https://localhost:8000/category", // new url after remove all options
+//         ],
+//         // color
+//         [
+//             "title" => "ویژگی چک باکس : موجود",
+//             "url" => "https://localhost:8000/category?colorBox=black", // new url after remove this option
+//         ],
+//         // material
+//         [
+//             "title" => "ویژگی چک باکس : همه",
+//             "url" => "https://localhost:8000/category?colorBox=red", // new url after remove this option
+//         ],
+//     ];
+
+//     // total is the total number of pages
+//     return response()->json(['html' => $html,'total' => 10,'removeOptions' => $removeOptions]);
+// });
 
