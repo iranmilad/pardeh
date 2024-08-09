b0391857b8767b383b51d3052681e78c0e2d555d
Farhad Bagheri <coding.farhad@gmail.com>
2024-08-09 15:17:09 +0330

    4 font families added

--- resources/views/layouts/primary.blade.php
+++ resources/views/layouts/primary.blade.php
@@ -13,7 +13,13 @@
 
   @livewireStyles
   @vite('resources/css/vendors/bootstrap/bootstrap.rtl.css')
+  @vite('resources/fonts/iranyekan/font.css')
   @vite('resources/css/global-style.css')
+  <style>
+    :root{
+      --font-family: IranYekan , "Aldhabi";
+    }
+  </style>
   @vite('resources/css/vendors/fontawesome/fontawesome.css')
   @vite('resources/css/style.css')
   @vite('resources/css/app.scss')
@@ -39,45 +45,45 @@
   @yield('js')
 
   <script>
-      function countdown(targetTimestamp) {
-        return {
-          hours: 0,
-          minutes: 0,
-          seconds: 0,
-          get formattedHours() {
-            return String(this.hours).padStart(2, '0');
-          },
-          get formattedMinutes() {
-            return String(this.minutes).padStart(2, '0');
-          },
-          get formattedSeconds() {
-            return String(this.seconds).padStart(2, '0');
-          },
-          targetDate: new Date(targetTimestamp),
-          startCountdown() {
+    function countdown(targetTimestamp) {
+      return {
+        hours: 0,
+        minutes: 0,
+        seconds: 0,
+        get formattedHours() {
+          return String(this.hours).padStart(2, '0');
+        },
+        get formattedMinutes() {
+          return String(this.minutes).padStart(2, '0');
+        },
+        get formattedSeconds() {
+          return String(this.seconds).padStart(2, '0');
+        },
+        targetDate: new Date(targetTimestamp),
+        startCountdown() {
+          this.updateCountdown();
+          setInterval(() => {
             this.updateCountdown();
-            setInterval(() => {
-              this.updateCountdown();
-            }, 1000);
-          },
-          updateCountdown() {
-            const now = new Date();
-            const diff = this.targetDate - now;
+          }, 1000);
+        },
+        updateCountdown() {
+          const now = new Date();
+          const diff = this.targetDate - now;
 
-            if (diff <= 0) {
-              this.hours = 0;
-              this.minutes = 0;
-              this.seconds = 0;
-              return;
-            }
+          if (diff <= 0) {
+            this.hours = 0;
+            this.minutes = 0;
+            this.seconds = 0;
+            return;
+          }
 
-            this.hours = Math.floor(diff / (1000 * 60 * 60));
-            this.minutes = Math.floor((diff % (1000 * 60 * 60)) / (1000 * 60));
-            this.seconds = Math.floor((diff % (1000 * 60)) / 1000);
-          },
-        };
-      }
-    </script>
+          this.hours = Math.floor(diff / (1000 * 60 * 60));
+          this.minutes = Math.floor((diff % (1000 * 60 * 60)) / (1000 * 60));
+          this.seconds = Math.floor((diff % (1000 * 60)) / 1000);
+        },
+      };
+    }
+  </script>
 </body>
 
 </html>
